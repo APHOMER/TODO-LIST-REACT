c399be0b33c06eaf68498a741027e8dc7730e3ba
Aphomer <aaphomer@gmail.com>
2024-04-24 20:25:30 +0100

    commented some lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import CssBaseline from "@mui/material/CssBaseline";
 import './App.css'
 import Navbar from './Navbar';
@@ -8,7 +8,7 @@ import Navbar from './Navbar';
 import TodoList from './Todolist';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>

b54ecf17dae3b3d23a1e7790e6b68bfedb164c61
Aphomer <aaphomer@gmail.com>
2024-04-25 11:08:11 +0100

    commented unuseful lines

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -48,23 +48,23 @@ export default function TodoForm({ addTodo }) {
 
 
 
-{/* <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            // endAdornment={
-            //   <InputAdornment position="end">
-            //     <IconButton
-            //       aria-label="toggle password visibility"
-            //       onClick={handleClickShowPassword}
-            //       onMouseDown={handleMouseDownPassword}
-            //       edge="end"
-            //     >
-            //       {showPassword ? <VisibilityOff /> : <Visibility />}
-            //     </IconButton>
-            //   </InputAdornment>
-            // }
-            label="Password"
-          /> */}
+// {/* <OutlinedInput
+//             id="outlined-adornment-password"
+//             type={showPassword ? 'text' : 'password'}
+//             // endAdornment={
+//             //   <InputAdornment position="end">
+//             //     <IconButton
+//             //       aria-label="toggle password visibility"
+//             //       onClick={handleClickShowPassword}
+//             //       onMouseDown={handleMouseDownPassword}
+//             //       edge="end"
+//             //     >
+//             //       {showPassword ? <VisibilityOff /> : <Visibility />}
+//             //     </IconButton>
+//             //   </InputAdornment>
+//             // }
+//             label="Password"
+//           /> */}
 
 
 

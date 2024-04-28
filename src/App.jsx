8ef4be2ba04df52828c91fee409b59b12c536986
Aphomer <aaphomer@gmail.com>
2024-04-28 21:33:39 +0100

    commentted usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import CssBaseline from "@mui/material/CssBaseline";
@@ -6,6 +6,7 @@ import './App.css'
 import Navbar from './Navbar';
 
 import TodoList from './Todolist';
+import SimpleBottomNavigation from "./SimpleBottomNavigation";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -16,6 +17,8 @@ function App() {
       <Navbar />
       {/* <h1>Helloooooooo</h1> */}
       <TodoList />
+      {/* <TableFooter /> */}
+      <SimpleBottomNavigation />
     </>
   )
 }

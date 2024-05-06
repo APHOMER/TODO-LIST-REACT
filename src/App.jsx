ac89cf37f46bfdbd85c7cd2309ffe3ce913d11fe
Aphomer <aaphomer@gmail.com>
2024-05-06 15:26:01 +0100

    included react logo and vielogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
 import CssBaseline from "@mui/material/CssBaseline";
 import './App.css'
 import Navbar from './Navbar';

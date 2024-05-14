7597115fda49ce7919041b8682d83f65faf6b077
Aphomer <aaphomer@gmail.com>
2024-05-14 15:29:44 +0100

    uncommented useSTate

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import TodoList from './Todolist';
 import SimpleBottomNavigation from "./SimpleBottomNavigation";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
